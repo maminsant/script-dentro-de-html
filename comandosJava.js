46e3826af141749d8ba3452dc91df0c6013fc7d3
maminsant <91914015+maminsant@users.noreply.github.com>
2022-01-17 17:43:37 -0300

    Update comandosJava.js

--- comandosJava.js
+++ comandosJava.js
@@ -3,6 +3,7 @@
     const topicos = new Array();
     subtitulos.forEach(subtitulo => {
         topicos.push(subtitulo.textContent);
+        subtitulo.innerHTML `${subtitulo.textContent} <a href='#'>início</a>`;
     });
     lista = document.querySelector('ol');
     topicos.forEach(topico => {
@@ -10,3 +11,4 @@
         paradinha.innerHTML = '<a href="#'+topico+'">' +topico+ '</a>';
         lista.append(paradinha);
     });
+
